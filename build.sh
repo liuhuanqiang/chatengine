b6d25e9dc904b2548d75c11ad6317cda2222f508
刘焕强 <837686721@qq.com>
2019-04-23 17:02:39 +0800

    u

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 export GO111MODULE=on
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/frontend ./access/frontend
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/auth_key ./access/auth_key
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/auth_session ./service/auth_session
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/sync ./messenger/sync
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/upload ./messenger/upload
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/document ./service/document
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/biz_server ./messenger/biz_server
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./bin/session ./access/session
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./access/frontend/frontend ./access/frontend
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./access/auth_key/auth_key ./access/auth_key
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./service/auth_session/auth_session ./service/auth_session
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./messenger/sync/sync ./messenger/sync
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./messenger/upload/upload ./messenger/upload
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./service/document/document ./service/document
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./messenger/biz_server/biz_server ./messenger/biz_server
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build  -o ./access/session/session ./access/session
